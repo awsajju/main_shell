02b82a151d1de953191659d16329c8e73fc8542f
awsajju <aws.devops.123aws@gmail.com>
2025-01-02 04:17:24 +0530

    varibale with args

---
+++ variable-args.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+Name1=$1
+Name2=$2
+
+echo "$Name1,hi $Name2 how are u" 
+echo "$Name2, I $Name1 I'm good and Thank u for asking and u"
+echo "$Name1, Great I'm also good"
+echo "$Name2, How is your day"
